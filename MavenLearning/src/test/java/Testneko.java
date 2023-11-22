d5f0adc865f293163c52a4c5029b1372b6bc80f8
shenchenhang <shenchenhang@58.com>
2023-11-22 16:15:04 +0800

    maven learning auto clean and maven properties binding
    miao
    
    must exelude any target file

---
+++ MavenLearning/src/test/java/Testneko.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class Testneko {
+
+    @Test
+    public void test(){
+        for(int i = 0; i < 20; i++){
+            System.out.println("121312321!!!");
+        }
+//        assert 1 == 0;
+    }
+}
---
+++ MavenLearning/src/test/java/test.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class test {
+
+    @Test
+    public void test(){
+        for(int i = 0; i < 10; i++){
+            System.out.println("121312321!!!");
+        }
+//        assert 1 == 0;
+    }
+}
